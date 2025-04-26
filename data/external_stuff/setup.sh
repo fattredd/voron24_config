3c75288d537b2dd66ef7f65f37072f2b672817b8
JButler <fattredd@gmail.com>
2025-04-26 14:42:12 -0400

    CPU affinity

--- data/external_stuff/setup.sh
+++ data/external_stuff/setup.sh
@@ -8,4 +8,10 @@ set -e
 cur_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # KlipperScreen
-ln -s $(cur_dir)/light-on.svg ~/KlipperScreen/styles/z-bolt/images/light-on.svg
+ln -sf ${cur_dir}/light-on.svg ~/KlipperScreen/styles/z-bolt/images/light-on.svg
+
+# Klipper CPU Affinity
+sudo mkdir -p /etc/systemd/system.conf.d/
+sudo mkdir -p /etc/systemd/system/klipper.service.d
+sudo ln -sf ${cur_dir}/cpuaffinity.conf /etc/systemd/system.conf.d/cpuaffinity.conf
+sudo ln -sf ${cur_dir}/override.conf /etc/systemd/system/klipper.service.d/override.conf
